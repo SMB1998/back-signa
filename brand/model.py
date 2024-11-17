33334ac26c7296705a5c9c6e7a6493555b1be0e6
Santiago MB <santiagomnoz318@gmail.com>
2024-11-17 16:50:17 -0500

    .

--- brand/model.py
+++ brand/model.py
@@ -3,8 +3,8 @@
 from django.db import models
 
 class Brand(models.Model):
-    name = models.CharField(max_length=255)
-    owner = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, null=True, blank=True)
+    owner = models.CharField(max_length=255, null=True, blank=True)
     status = models.CharField(max_length=50, default='in_process')  
 
     def __str__(self):
