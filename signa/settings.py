315dfc7658dffced20e89d4829d1b34ee6ce6ae4
Santiago MB <santiagomnoz318@gmail.com>
2024-11-17 13:32:48 -0500

    .

--- signa/settings.py
+++ signa/settings.py
@@ -13,7 +13,7 @@ DEBUG = os.getenv('DEBUG') == 'True'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
-ALLOWED_HOSTS = ['signa-qb4n.onrender.com', 'loaclhost', '127.0.0.1']
+ALLOWED_HOSTS = ['signa-qb4n.onrender.com', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     
@@ -52,5 +52,11 @@ DATABASES = {
 }
 
 ROOT_URLCONF = 'signa.urls'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
+}
 
 
